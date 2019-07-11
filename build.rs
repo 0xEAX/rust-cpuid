6b4876c2b4a77bd77e54cc4cacca090b9d82dfe1
0xEAX <51427116+0xEAX@users.noreply.github.com>
2019-07-11 17:40:33 -0400

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,5 +1,2 @@
 extern crate cc;
-extern crate rustc_version;
-use rustc_version::{version, version_meta, Channel, Version};
-
 fn main() { cc::Build::new().file("src/cpuid.c").compile("libcpuid.a"); }
